f15b71d230104a036a7a9f73aafeb85db74dc431
Guillermo <49098836+GuillermoBustosPerez@users.noreply.github.com>
2020-08-09 12:02:13 +0200

    Delete ETS, ARIMA and DHR (Fourier) predictions.R

--- Forecasting (predicción en series temporales)/ETS, ARIMA and DHR (Fourier) predictions.R
+++
@@ -1,7 +0,0 @@
-#### Load data and make it into a ts ####
-load("Data/Monthly sales.RData")
-library(tidyverse); library(forecast); library(fpp2)
-
-M_Sales <- ts(M_Sales, 
-              start = c(2013, 2),
-              frequency = 12)
